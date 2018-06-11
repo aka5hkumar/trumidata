bf1008228ccbf44218af95b7821820101c80698c
Akash Kumar <aka5hkumar@users.noreply.github.com>
2018-06-10 23:29:50 -0400

    quick patch for tomorrow
    will resume!

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// const { Client } = require('pg');
-//
-// const client = new Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: true,
-// });
-//
-// client.connect();
-//
-// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-//   if (err) throw err;
-//   for (let row of res.rows) {
-//     console.log(JSON.stringify(row));
-//   }
-//   client.end();
-// });
+
 class App extends Component {
   render() {
     return (
